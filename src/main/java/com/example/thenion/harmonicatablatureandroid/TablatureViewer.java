ddf286983b1ccb2066e3da6f9879c64cddf83747
TOURNIER Maxime <steaker90@gmail.com>
2017-09-25 17:57:37 +0200

    Project
    Add project files

---
+++ src/main/java/com/example/thenion/harmonicatablatureandroid/TablatureViewer.java
@@ -0,0 +1,13 @@
+package com.example.thenion.harmonicatablatureandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TablatureViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablature_viewer);
+    }
+}
